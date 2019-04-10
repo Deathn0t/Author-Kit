3b004c143044087084b507435eb14ce84a5ea946
Deathn0t <romainegele@gmail.com>
2019-04-10 21:43:54 +0000

    update

---
+++ qsub_script_theta_os.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#COBALT -n 1
+#COBALT -t 30
+#COBALT -A datascience
+
+echo "Starting Cobalt job script"
+export n_nodes=$COBALT_JOBSIZE
+export n_mpi_ranks_per_node=1
+export n_mpi_ranks=1
+#export n_openmp_threads_per_rank=128
+export n_hardware_threads_per_core=2
+#export n_hardware_threads_skipped_between_ranks=4
+
+source activate dh-cray
+
+aprun -n $n_mpi_ranks -N $n_mpi_ranks_per_node \
+      -j $n_hardware_threads_per_core sh script_collect_os.sh 
